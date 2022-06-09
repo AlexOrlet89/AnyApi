e6b75e480cd1042d402132a9e1323870c1b7db97
Alex Orlet <AlexOrlet89@gmail.com>
2022-06-09 11:12:26 -0700

    friends/1 test complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,8 +7,10 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/friends/1 should return ross', async () => {
+    const results = await request(app).get('/friends/1');
+    const expected = { id: '1', name: 'Ross', status: 'Alive' };
+    expect(results.body).toEqual(expected);
   });
   afterAll(() => {
     pool.end();
